a651bfede4c4981ba616b1de1bcefaba6e6529aa
barrystyle <barrystyle@westnet.com.au>
2019-02-13 09:53:02 +0000

    print block attempts but dont log to rejected

--- client_submit.cpp
+++ client_submit.cpp
@@ -255,12 +255,9 @@ static void client_do_submit(YAAMP_CLIENT *client, YAAMP_JOB *job, YAAMP_JOB_VAL
 		}
 
 		else {
+			printf("block:    %s\n", block_hex); 
 			debuglog("*** REJECTED :( %s block %d %d txs\n", coind->name, templ->height, templ->txcount);
-			rejectlog("REJECTED %s block %d\n", coind->symbol, templ->height);
-			if (g_debuglog_hash) {
-				//debuglog("block %s\n", block_hex);
-				debuglog("--------------------------------------------------------------\n");
-			}
+			// rejectlog("REJECTED %s block %d\n", coind->symbol, templ->height);
 		}
 	}
 
